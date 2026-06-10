ab9b7408805e590163eff2a7879f89187c69b2fa
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-10 21:27:51 +0530

    load PostgreSQL database connection URL from environment variables

--- src/config.rs
+++ src/config.rs
@@ -9,6 +9,7 @@ pub struct Config {
 impl Config {
 
     pub fn init() -> Config {
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         
     }
     
