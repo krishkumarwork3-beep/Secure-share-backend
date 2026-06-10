3441165a6c33c021457a80daa633cf6b53ff1658
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-10 21:58:27 +0530

    add core authentication and identity fields to user model

--- src/models.rs
+++ src/models.rs
@@ -3,5 +3,9 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Deserialize, Serialize, sqlx::FromRow, sqlx::Type)]
 pub struct User {
-    
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+   
 }
