7dcf2ede37627f5077b657d33b0bd86332ee469d
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-11 15:08:19 +0530

    serialize structured error responses into JSON output

--- src/error.rs
+++ src/error.rs
@@ -13,6 +13,6 @@ pub struct ErrorResponse {
 
 impl fmt::Display for ErrorResponse {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-
+        write!(f, "{}", serde_json::to_string(&self).unwrap())
     }
 }
