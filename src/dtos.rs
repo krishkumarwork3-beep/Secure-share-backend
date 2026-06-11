257c5a0d56659d480336708f38b8191e2be7cc52
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-11 10:24:32 +0530

    include sent file creation timestamp in response DTO

--- src/dtos.rs
+++ src/dtos.rs
@@ -97,6 +97,7 @@ impl UserSendFileDto {
             file_name: file_data.file_name.to_owned(),
             recipient_email: file_data.recipient_email.to_owned(),
             expiration_date: file_data.expiration_date.unwrap(),
+            created_at: file_data.created_at.unwrap(),
         }
     }
 
