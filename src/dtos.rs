09d58c7aad17d1118af81d4ba756acf65377c477
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-11 09:57:07 +0530

    add password validation to login DTO

--- src/dtos.rs
+++ src/dtos.rs
@@ -32,5 +32,9 @@ pub struct RegisterUserDto {
 pub struct LoginUserDto {
     #[validate(length(min = 1, message = "Email is required"), email(message = "Email is invalid"))]
     pub email: String,
-    
+    #[validate(
+        length(min = 1, message = "Password is required"),
+        length(min = 6, message = "Password must be at least 6 characters")
+    )]
+    pub password: String,
 }
