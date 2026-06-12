3463abc2a829e35c374c5c17886406ed6049e67d
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-12 10:10:45 +0530

    create jwt token claims struct

--- src/utils/token.rs
+++ src/utils/token.rs
@@ -3,3 +3,10 @@ use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation}
 use serde::{Deserialize, Serialize};
 
 use crate::error::{ErrorMessage, HttpError};
+
+#[derive(Debug, Serialize, Deserialize,)]
+pub struct TokenClaims {
+    pub sub: String,
+    pub iat: usize,
+    pub exp: usize,
+}
