52f1a10df96f0e889f6885268f95becb671dc8a5
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-12 09:11:55 +0530

    add database cors logging and scheduler imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 mod config;
 mod models;
 mod dtos;
@@ -13,3 +14,11 @@ use axum::{
     },
     Router
 };
+
+use config::Config;
+use db::{DBClient, UserExt};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use tower_http::cors::CorsLayer;
+use tracing_subscriber::filter::LevelFilter;
+use tokio_cron_scheduler::{JobScheduler, Job};
