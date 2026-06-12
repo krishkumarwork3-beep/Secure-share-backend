68b7ec0c526b8fb6a8083586d7e29f5c480f3147
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-06-12 09:17:06 +0530

    configure cors middleware

--- src/main.rs
+++ src/main.rs
@@ -52,4 +52,9 @@ async fn main() {
             }
         };
 
+        let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
+        .allow_credentials(true)
+        .allow_methods([Method::GET, Method::POST, Method::PUT]);
 }
