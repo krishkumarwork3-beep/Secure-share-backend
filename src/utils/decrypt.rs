a38df45e7ee3c13b79ae9d43e6bd27d3f40dc866
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-20 21:58:54 +0530

    add file decryption function interface

--- src/utils/decrypt.rs
+++ src/utils/decrypt.rs
@@ -3,3 +3,9 @@ use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey};
 
 use crate::error::HttpError;
+pub async fn decrypt_file(
+    encrypted_aes_key: Vec<u8>,
+    encrypted_file_data: Vec<u8>,
+    iv: Vec<u8>,
+    user_private_key: &RsaPrivateKey,
+) -> Result<Vec<u8>, HttpError> {
