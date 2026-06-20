b644d64f469832b1cea866f34a4c397efd295f49
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-20 21:59:09 +0530

    decrypt AES key using RSA private key

--- src/utils/decrypt.rs
+++ src/utils/decrypt.rs
@@ -9,3 +9,8 @@ pub async fn decrypt_file(
     iv: Vec<u8>,
     user_private_key: &RsaPrivateKey,
 ) -> Result<Vec<u8>, HttpError> {
+    let aes_key = user_private_key.decrypt(
+    Pkcs1v15Encrypt,
+    &encrypted_aes_key
+)
+.map_err(|e| HttpError::server_error(e.to_string()))?;
