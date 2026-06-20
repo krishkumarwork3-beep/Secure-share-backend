d57b26eda8b8f79aa8824c4b075bd69e15294d6b
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-20 21:40:18 +0530

    add imports for JWT authentication middleware

---
+++ src/middleware.rs
@@ -0,0 +1,7 @@
+use std::sync::Arc;
+
+use axum::{extract::Request, http::header, middleware::Next, response::IntoResponse, Extension};
+use serde::{Deserialize, Serialize};
+use axum_extra::extract::cookie::CookieJar;
+
+use crate::{db::UserExt, error::{ErrorMessage, HttpError}, models::User, utils::token, AppState};
