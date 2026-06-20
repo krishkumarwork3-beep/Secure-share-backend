6fbe1c4bbe5f67918c2e141f2b92be4b8d38f8a1
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-20 21:50:26 +0530

    add file encryption function interface

--- src/utils/encrypt.rs
+++ src/utils/encrypt.rs
@@ -4,3 +4,7 @@ use rand::Rng;
 use rsa::{Pkcs1v15Encrypt, RsaPublicKey};
 
 use crate::error::HttpError;
+pub async fn encrypt_file(
+    file_data: Vec<u8>,
+    user_public_key: &RsaPublicKey
+) -> Result<(Vec<u8>, Vec<u8>, Vec<u8>), HttpError> {
