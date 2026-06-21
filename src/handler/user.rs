717e3da583c0d27b88842a9bdd75a6474d42cb3e
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-21 06:47:33 +0530

    add dependencies for user management handlers

---
+++ src/handler/user.rs
@@ -0,0 +1,23 @@
+use std::sync::Arc;
+
+use axum::{extract::Query, response::IntoResponse, routing::{get, put}, Extension, Json, Router};
+use validator::Validate;
+
+use crate::{
+    db::UserExt,
+    dtos::{
+        EmailListResponseDto,
+        FilterEmailDto,
+        FilterUserDto,
+        NameUpdateDto,
+        Response,
+        SearchQueryByEmailDTO,
+        UserData,
+        UserPasswordUpdateDto,
+        UserResponseDto
+    },
+    error::{ErrorMessage, HttpError},
+    middleware::JWTAuthMiddeware,
+    utils::password,
+    AppState
+};
