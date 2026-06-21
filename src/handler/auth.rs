61b1611a9bc7e52958d22ffbeba24f739880fced
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-21 06:26:50 +0530

    add dependencies for authentication handlers

---
+++ src/handler/auth.rs
@@ -0,0 +1,7 @@
+use std::sync::Arc;
+
+use axum::{http::{header, HeaderMap, StatusCode}, response::IntoResponse, routing::post, Extension, Json, Router};
+use axum_extra::extract::cookie::Cookie;
+use validator::Validate;
+
+use crate::{db::UserExt, dtos::{LoginUserDto, RegisterUserDto, Response, UserLoginResponseDto}, error::{ErrorMessage, HttpError}, utils::{keys::generate_key, password, token}, AppState};
