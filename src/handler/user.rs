fa5abb93134f847ab523241186a0a84ff5c856cf
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-21 06:54:45 +0530

    extract authenticated user identity

--- src/handler/user.rs
+++ src/handler/user.rs
@@ -86,3 +86,6 @@ pub async fn update_user_password(
 ) -> Result<impl IntoResponse, HttpError> {
     body.validate()
        .map_err(|e| HttpError::bad_request(e.to_string()))?;
+    let user = &user.user;
+
+    let user_id = uuid::Uuid::parse_str(&user.id.to_string()).unwrap();
