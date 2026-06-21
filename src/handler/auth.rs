f84b350fae36b9e0558a2333f4ce6a7d698f9fc8
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-21 06:37:13 +0530

    fetch user record by email during login

--- src/handler/auth.rs
+++ src/handler/auth.rs
@@ -48,3 +48,7 @@ pub async fn login(
 ) -> Result<impl IntoResponse, HttpError> {
         body.validate()
         .map_err(|e| HttpError::bad_request(e.to_string()))?;
+        let result = app_state.db_client
+        .get_user(None, None, Some(&body.email))
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
