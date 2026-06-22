c44f2c4a5932576a465520ff26496173c406c523
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-22 06:46:16 +0530

    process multipart uploads and validate file metadata

--- src/handler/file.rs
+++ src/handler/file.rs
@@ -50,3 +50,50 @@ pub fn file_handle() -> Router {
         .route("/upload", post(upload_file))
         .route("/retrieve", post(retrieve_file))
 }
+
+pub async fn upload_file(
+    Extension(app_state): Extension<Arc<AppState>>,
+    Extension(user): Extension<JWTAuthMiddeware>,
+    mut multipart: Multipart
+) -> Result<impl IntoResponse, HttpError> {
+
+    let mut file_data = Vec::new();
+    let mut file_name = String::new();
+    let mut file_size: i64 = 0;
+
+    let mut form_data = FileUploadDtos {
+        recipient_email: String::new(),
+        password: String::new(),
+        expiration_date: String::new(),
+    };
+
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+
+        match name.as_str() {
+            "fileUpload" => {
+                file_name = field.file_name()
+                    .unwrap_or("unknow_file")
+                    .to_string();
+
+                file_data = field.bytes().await.unwrap().to_vec();
+                file_size = file_data.len() as i64;
+            }
+            "recipient_email" => {
+                form_data.recipient_email =
+                    field.text().await.unwrap();
+            }
+            "password" => {
+                form_data.password =
+                    field.text().await.unwrap();
+            }
+            "expiration_date" => {
+                form_data.expiration_date =
+                    field.text().await.unwrap();
+            }
+            _ => {}
+        }
+    }
+
+    form_data.validate()
+        .map_err(|e| HttpError::bad_request(e.to_string()))?;
